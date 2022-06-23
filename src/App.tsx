467f0f5ba4ae7a80fbae0917c5263d2a9ef1df12
Julian Ribeiro <Juliansr.ie@gmail.com>
2022-06-23 01:45:21 -0300

    ReactJS 1.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-function App() {
-  const { data } = useQuery<{ lessons : Lesson[]}>(GET_LESSONS_QUERY)
-
-  /* Using API with useEffect
-  useEffect(() => {
-    client.query({
-      query: GET_LESSONS_QUERY,
-    }).then(response => {
-      console.log(response.data)
-    })
-  }, [])
-  */
-
+function App() { 
   return (
-    <ul>                
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
